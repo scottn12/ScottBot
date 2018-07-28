7dc0c0123298d1c6c7889d4dbc13498d3133ed59
Coext <scott.norton12@gmail.com>
2018-07-28 04:32:55 -0400

    token

--- bot.py
+++ bot.py
@@ -15,5 +15,4 @@ for extension in extensions:
 async def on_ready():
     print(bot.user.name + ' Version: ' + version + " is ready!")
 
-bot.run('NDY1OTY5NTU2NjMzMDI2NjA0.Djl3hg.hqURwT8lVPnyAWMRY8BN45iVu84')
-#bot.run(os.environ.get('BOT_TOKEN', None))
+bot.run(os.environ.get('BOT_TOKEN', None))
--- misc.py
+++ misc.py
@@ -18,20 +18,7 @@ class Misc:
         name = ctx.message.author.mention
         await self.bot.say("Hello {}!".format(name))
 
-    @commands.command(pass_context=True)
-    async def clear(self, ctx):
-        '''Deletes all commands and messages sent from ScottBot.'''
-
-        messages = []
-        async for message in self.bot.logs_from(ctx.message.channel):
-            if (ctx.message.author == self.bot.user or ctx.message.content[0]=='!'):
-                messages.append(message)
-        if len(messages) > 1:
-            await self.bot.delete_messages(messages)
-
-        # For messages older than 14 days
-        async for message in messages:
-            await self.bot.delete_message(message)
+    
 
 def setup(bot):
     bot.add_cog(Misc(bot))
